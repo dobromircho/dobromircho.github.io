f30a8a8522ab51132a969649124e1be64447a09c
Dobromir <dobromircho79@gmail.com>
2018-03-20 12:17:34 +0200

    Design changes

---
+++ JavaScripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$("#shniciPic").hide();
+	$("#shnici").mouseenter(function(){
+		$("#shniciPic").show(200);
+	});
+	$("#shnici").mouseleave(function(){
+		$("#shniciPic").hide(400);
+	});
+  /* $("#menu").onmouseover(function(){
+	  $(this).css("background-color","red");
+  }); */
+
+  
+
+});
