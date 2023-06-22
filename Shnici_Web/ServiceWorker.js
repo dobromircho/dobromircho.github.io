ba5345e0a87dce37e54e0b504e60878ec1c4b8bf
Dobromir <dobromircho79@gmail.com>
2023-06-22 21:54:42 +0300

    English version

---
+++ Shnici_Web/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "JDgames-Shnici-1.0";
+const contentToCache = [
+    "Build/Shnici_HTML5_ENGL.loader.js",
+    "Build/Shnici_HTML5_ENGL.framework.js.unityweb",
+    "Build/Shnici_HTML5_ENGL.data.unityweb",
+    "Build/Shnici_HTML5_ENGL.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
